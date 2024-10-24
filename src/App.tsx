5f15faf08d6989cc6f88926f2c12642eba47de7a
Hyerim Yang <hlhlstar@naver.com>
2024-10-25 01:21:35 +0900

    Fix: 페이지 중복 호출 방지 로직 추가
    - 페이지 호출 히스토리 관리용 배열(`pageParams`) 추가
    - 데이터 불러온 후 pageParams에 해당 페이지 번호 저장
    - 이미 호출된 페이지 번호 중복 호출 방지 처리

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,17 @@ const App = () => {
   const [page, setPage] = useState(0); // 현재 페이지
   const [loading, setLoading] = useState(false);
   const [hasNextPage, setHasNextPage] = useState(false); // 다음 페이지 없으면 무한 스크롤 호출 X
+  const [pageParams, setPageParams] = useState<Number[]>([]); // 페이지 호출 히스토리 관리
 
   const fetchProducts = async () => {
+    if (pageParams.includes(page)) return; // 페이지 중복 호출 방지
+
     setLoading(true); // 데이터 불러오는 동안 로딩 상태 유지
 
     const { datas, isEnd } = await getMockData(page); // page에 해당하는 데이터, 다음 페이지 유무 받아오기
 
     setProducts((prevProducts) => [...prevProducts, ...datas]); // 기존 데이터에 새로운 페이지 데이터 이어 붙이기
+    setPageParams((prev) => [...prev, page]); // 호출한 페이지 번호 저장
     setHasNextPage(!isEnd); // 다음 페이지 유무 상태 설정
     setLoading(false); // 데이터 페칭 및 처리 완료, 로딩 상태 종료
   };
